d194b597588986cc06f62a2a798198a44accb157
Ralph Lavaud <rlavaud@gmail.com>
2025-09-28 07:34:45 -0400

    Fix: Pull filebeatdynamically to build fix error when running arm64 architecture

--- build-docker-images/wazuh-manager/config/filebeat_module.sh
+++ build-docker-images/wazuh-manager/config/filebeat_module.sh
@@ -7,6 +7,19 @@ if [[ -n "${WAZUH_TAG}" ]]; then
   REPOSITORY="packages.wazuh.com/4.x"
 fi
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}-x86_64.rpm &&\
-yum install -y ${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}-x86_64.rpm && rm -f ${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}-x86_64.rpm && \
+function getArchBasedFilebeatVersion() {
+  case $(arch) in
+    'aarch64' | 'arm64') echo ${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}"-aarch64.rpm"   ;;
+    'x86_64'  | 'amd64') echo ${FILEBEAT_CHANNEL}-${FILEBEAT_VERSION}"-x86_64.rpm"     ;;
+    *)
+      echo "Architecture $(arch) not supported" >> /dev/stderr;
+      exit 1;
+    ;;
+  esac
+}
+
+FILEBEAT_FILE=$(getArchBasedFilebeatVersion);
+
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/${FILEBEAT_FILE} &&\
+yum install -y ${FILEBEAT_FILE} && rm -f ${FILEBEAT_FILE} && \
 curl -s https://${REPOSITORY}/filebeat/${WAZUH_FILEBEAT_MODULE} | tar -xvz -C /usr/share/filebeat/module
